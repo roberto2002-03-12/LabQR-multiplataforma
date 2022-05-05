369ae2e7f5623e68077134b58636dde2f4678891
roberto2002-03-12 <67761057+roberto2002-03-12@users.noreply.github.com>
2022-05-05 10:17:52 -0500

    Agregar archivos de proyecto.

---
+++ LabQR/LabQR/Services/IQrScanningService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabQR.Services
+{
+    public interface IQrScanningService
+    {
+        Task<string> ScanAsync();
+    }
+}
